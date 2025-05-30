49fcc48867c7e5e04cb7903cc740f2bf7629a339
Iamfitz <vuitsyk.a@gmail.com>
2025-05-30 18:05:41 +0300

    update Readme and add some logic for extracting more information about Users in public API

--- Program.cs
+++ Program.cs
@@ -11,14 +11,31 @@ var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 var usersRAM = JsonSerializer.Deserialize<List<User>>(usersJsonData, options)!;
 var postsRAM = JsonSerializer.Deserialize<List<Post>>(postsJsonData, options)!;
 
-var filteredUsers = usersRAM
+var filteredUsersByS = usersRAM
     .Where(u => u.Address.City.StartsWith("S", StringComparison.OrdinalIgnoreCase))
     .ToList();
 var sortedUsersByPost = usersRAM
     .OrderByDescending(u => postsRAM.Count(m => m.UserId == u.Id))
     .ToList();
+var topUserData = usersRAM
+    .Select(u => new {
+        User = u,
+        PostCount = postsRAM.Count(p => p.UserId == u.Id)
+    })
+    .OrderByDescending(x => x.PostCount)
+    .First();
+var topThreeUsers = usersRAM
+    .Select(u => new {
+        Name = u.Name,
+        PostCount = postsRAM.Count(p => p.UserId == u.Id)
+    })
+    .OrderByDescending(x => x.PostCount)
+    .Take(3)
+    .ToList();
 
-foreach (var user in filteredUsers) {
+
+Console.WriteLine("Extract people who's city Starting with S");
+foreach (var user in filteredUsersByS) {
     var userPosts = postsRAM
         .Where(p => p.UserId == user.Id)
         .ToList();
@@ -29,12 +46,18 @@ foreach (var user in filteredUsers) {
     Console.WriteLine($"Posts count: {userPosts.Count}");
     Console.WriteLine(new string('-', 22));
 }
+Console.WriteLine("Extract people sorted by Post Count");
 foreach (var user in sortedUsersByPost) {
     Console.WriteLine($"{user.Name}");
     Console.WriteLine($"City: {user.Address.City}");
     Console.WriteLine(new string('-', 22));
 }
-var topUser = usersRAM
-    .OrderByDescending(u => postsRAM.Count(p => p.UserId == u.Id))
-    .First();
-Console.WriteLine($"The top user by Post is : {topUser.Name}");
+Console.WriteLine(new string('-', 22));
+//Extract 1 person who has the most posts
+Console.WriteLine($"Top user: {topUserData.User.Name} with {topUserData.PostCount} posts");
+
+Console.WriteLine(new string('-', 22));
+Console.WriteLine("Extract top 3 users by post cout");
+foreach (var user in topThreeUsers) {
+    Console.WriteLine($"{user.Name,-25} {user.PostCount} posts");
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ConsoleApplicationTest
+﻿# ConsoleApplicationTest (.NET 8.0)
+
+A simple .NET console application that fetches users and posts from a public API using LINQ.
+
+
+## How to Run the App
+
+1. **Clone the repository** or [download the ZIP](https://github.com/Iamfittz/ConsoleApplicationTest/archive/refs/heads/master.zip) and extract it.
+
+2. **Open a terminal and navigate to the project directory**: 
+
+3. **Run the app using the .NET CLI:**   
+ ```bash
+dotnet run
+ ```
+## What's inside?
+1. Filter persons whose city starts with "S";
+1. Shows number of posts per user;
+1. Sort users by number of posts;
+1. Show TOP 1 user by posts;
+1. Show a top 3 users by posts;
